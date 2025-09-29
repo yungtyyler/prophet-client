210eebab73f86ae7253989349397a6e7530f0670
Tyler Varzeas <tyler.varzeas@gmail.com>
2025-09-28 19:47:33 -0700

    Make home page display teams for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,69 @@
 import Image from "next/image";
+import React from "react";
 
-export default function Home() {
+type NFLTeam = {
+  id: number; // primary key
+  name: string; // e.g. "Cowboys"
+  city: string; // e.g. "Dallas"
+  abbreviation: string; // e.g. "DAL"
+  conference: string; // e.g. "NFC"
+  division: string; // e.g. "East"
+  establishedYear?: number; // optional, if in table
+  logoUrl?: string; // optional, if stored
+};
+
+export default async function Home() {
+  console.log(process.env.NEXT_PUBLIC_API_URL);
+  const teams: NFLTeam[] = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/teams`, {
+    cache: "no-store",
+  }).then((res) => {
+    if (!res.ok) {
+      throw new Error("Failed to fetch teams.");
+    }
+    return res.json();
+  });
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="flex flex-col gap-4 max-w-6xl mx-auto p-8">
+      <h1 className="text-2xl font-bold text-center mb-4">All the teams in the NFL</h1>
+      <ul className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-x-2 gap-y-8">
+        {teams.map((team) => {
+          const { id, abbreviation, city, conference, division, name, establishedYear, logoUrl } =
+            team;
+          const imgUrl = logoUrl ? logoUrl.toString() : "/nfl_logo.png";
+          const year = establishedYear ? establishedYear.toString() : "Unknown";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          return (
+            <li key={id} className="col-span-1 flex flex-col gap-2">
+              <h2 className="text-lg font-bold text-center">{name}</h2>
+              <Image
+                src={imgUrl}
+                alt={`${name} Logo`}
+                width={50}
+                height={50}
+                className="max-w-[100px] w-full mx-auto"
+              />
+              <div className="flex flex-col gap-2 justify-start">
+                <p>
+                  <strong>Team Code:</strong> {abbreviation}
+                </p>
+                <p>
+                  <strong>City:</strong> {city}
+                </p>
+                <p>
+                  <strong>Conferene:</strong> {conference}
+                </p>
+                <p>
+                  <strong>Division:</strong> {division}
+                </p>
+                <p>
+                  <strong>Year Established:</strong> {year}
+                </p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
